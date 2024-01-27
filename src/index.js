740434b399b0765b2276b38742321470f5ce708d
toantran <boynhangheo2002hd@gmail.com>
2024-01-27 17:05:24 +0700

    nav

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://127.0.0.1:8000/graphql/",
+    uri: "https://ddfd-183-80-130-9.ngrok-free.app/graphql/",
     cache: new InMemoryCache(),
 });
 
