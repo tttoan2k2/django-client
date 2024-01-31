9816d2bee87d8fd970f51bfe8f19fd9346299492
HoangLoi1234 <Hoangloia16@gmail.com>
2024-01-31 13:16:15 +0700

    fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Nav = () => {
-    return <div className=" text-red-500">Hello World!</div>;
+    return <div className=" text-red-500 text-3xl">Hello World!</div>;
 };
 
 export default Nav;
